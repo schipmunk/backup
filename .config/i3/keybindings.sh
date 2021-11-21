196ca13e9b64787492606f7a9f8cc7c32c3364d9
Norman Hamann <boyscout399@yahoo.com>
2021-11-21 14:57:39 -0800

    fixed keybindings help script

--- .config/i3/keybindings.sh
+++ .config/i3/keybindings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -n '/START_KEYS/,/END_KEYS/p' ~/.config/i3/config | \
-    grep -v -e START_KEYS -v -e END_KEYS -v -e \#bindsym | \
+tail -n +4 ~/.config/i3/keybindings | \
+    grep -v -e \#bindsym | \
     yad --text-info --back=#2E3440 --fore=#D8DEE9 --geometry=1900x1040 --no-buttons --borders=0
