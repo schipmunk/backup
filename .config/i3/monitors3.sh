b801892510f523d3cfa5a63a0d4a4cd4365bce32
Norman Hamann <boyscout399@yahoo.com>
2023-02-14 00:16:49 -0500

    added 3 monitor script

---
+++ .config/i3/monitors3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xrandr  --output DP-4 --primary  --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal  --output DP-2 --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output HDMI-0 --mode 3840x2160 --rate 60  --pos 5120x0 --rotate normal
+#xrandr  --output DP-2 --primary  --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal # --output DP-2 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal
