2539a03e2083359e98ef0ad4877a3d8f347bbec5
Norman Hamann <boyscout399@yahoo.com>
2023-02-14 00:39:35 -0500

    fixing xrandr script

--- .config/i3/monitors2.sh
+++ .config/i3/monitors2.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-xrandr  --output DP-4 --primary  --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal  --output DP-2 --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal 
-#xrandr  --output DP-2 --primary  --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal # --output DP-2 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal
+xrandr  --output DP-4 --primary  --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal  --output DP-2 --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output HDMI-0 --off
