c9af4ac5d0e179e15410fcda4475c55192871270
Norman Hamann <boyscout399@yahoo.com>
2023-05-15 00:52:08 -0400

    minor updates

--- .config/i3/monitors3.sh
+++ .config/i3/monitors3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr  --output DP-4 --primary  --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal  --output DP-2 --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output HDMI-0 --mode 3840x2160 --rate 120  --pos 5120x0 --rotate normal
+xrandr  --output DP-4 --primary  --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal  --output DP-2 --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output HDMI-0 --mode 3840x2160 --rate 60  --pos 5120x0 --rotate normal
